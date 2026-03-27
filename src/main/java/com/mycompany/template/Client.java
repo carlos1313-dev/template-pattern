b73f9ef8e0950e4548d4e8760b42cf85f2c1bfe7
Carlos E <sangroniscarlosenrique1@gmail.com>
2026-03-27 00:52:20 -0500

    Add: archivos de ejemplo en la carpeta samples/

--- src/main/java/com/mycompany/template/Client.java
+++ src/main/java/com/mycompany/template/Client.java
@@ -4,13 +4,43 @@
 
 package com.mycompany.template;
 
+import com.mycompany.template.core.DataMiner;
+import com.mycompany.template.factory.DataMinerFactory;
+
 /**
+ * Punto de entrada de la aplicación de minería de datos corporativos.
  *
- * @author sangr
+ * Uso:
+ *   java dataminer.Client archivo1 archivo2 ...
+ *
+ * Si no se pasan argumentos, se procesan los archivos de muestra
+ * ubicados en ./samples/.
  */
 public class Client {
-
+ 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+ 
+        String[] archivos = (args.length > 0) ? args : new String[]{
+            "samples/informe_anual.doc",
+            "samples/ventas_q3.csv",
+            "samples/contrato_proveedor.pdf"
+        };
+ 
+        System.out.println("-------------------------------------------");
+        System.out.println("       Aplicación de Minería de Datos v2.0   ");
+        System.out.println("        Patrón de diseño: Template Method    ");
+        System.out.println("-------------------------------------------");
+ 
+        for (String archivo : archivos) {
+            DataMiner miner = DataMinerFactory.create(archivo);
+            if (miner != null) {
+                System.out.println("\n-------------------------------------------");
+                System.out.println(" Procesando: " + archivo);
+                System.out.println("---------------------------------------------");
+                miner.mine(archivo);     // ← Template Method en acción
+            }
+        }
+ 
+        System.out.println("\n Procesamiento finalizado.");
     }
 }
